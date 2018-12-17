e51f75441c5d6d8a11b7a4c8df99184824486112
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2018-12-17 11:13:15 +0100

    Fast fix of the wrong routing

--- server/index.js
+++ server/index.js
@@ -19,8 +19,8 @@ routes(app);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
 }
 
