44306e4e6daaf845406be293f79265607a53e227
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2018-12-22 00:04:26 +0100

    Finished implementation of the EmptyView component

--- client/src/components/Common/EmptyView/index.js
+++ client/src/components/Common/EmptyView/index.js
@@ -1,36 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const EmptyView = ({ section, description }) => {
-  if (description) {
-    return <div className="empty-view-container">{description}</div>;
-  }
+import './styles.css';
+
+const EmptyView = ({ section, children }) => {
+  const defaultDescription = (
+    <React.Fragment>
+      <p>
+        This section is dedicated to the information about my {section}.
+      </p>
+      <p>
+        For the moment you have not discovered anything regarding
+        this topic. To get some info - ask the bot right question.
+        When the bot gives you some related answer - the information will
+        appear on this page. Ask more questions to collect more info! Good luck!
+      </p>
+      <p>
+        If you are lazy and you do not want to play with the bot - here
+        you can find a link to
+        my <a href="https://www.linkedin.com/in/kovalenkodmytro/" target="_blank" rel="noopener noreferrer">LinkedIn</a> profile
+        and see everything at once.
+      </p>
+      <p>
+        Do not hesitate to contact me and share your opinion about this project!
+        I will appreciate a lot any feedback even if it is negative! Thank you in advance! :)
+      </p>
+    </React.Fragment>
+  );
 
   return (
     <div className="empty-view-container">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Sed sit amet elit ut diam fermentum mollis. In in risus nunc.
-      Donec tincidunt vulputate euismod. Ut non eros eros.
-      Mauris accumsan ligula a elit pretium, vitae placerat eros vulputate.
-      Nulla nulla ipsum, pellentesque ac malesuada sit amet, varius eget nisi.
-      Nunc dictum tempor condimentum. Vestibulum consequat porttitor massa sodales varius.
-      Donec placerat elementum turpis eget hendrerit. Integer et hendrerit libero,
-      in commodo arcu. Duis ac turpis sapien. Nullam blandit eu sapien in pharetra.
-      Mauris vestibulum nisl pulvinar eros suscipit ultricies. Curabitur a pretium arcu.
-      Nullam sapien magna, consequat sed magna at, gravida pellentesque risus.
-      Quisque vestibulum tortor eu risus elementum luctus.
+      { children || defaultDescription }
     </div>
   );
 };
 
 EmptyView.propTypes = {
   section: PropTypes.string,
-  description: PropTypes.string,
+  children: PropTypes.node,
 };
 
 EmptyView.defaultProps = {
   section: 'this section',
-  description: '',
+  children: null,
 };
 
 export default EmptyView;
--- client/src/components/Pages/Home/index.js
+++ client/src/components/Pages/Home/index.js
@@ -2,14 +2,35 @@ import React from 'react';
 
 import EmptyView from '../../Common/EmptyView';
 
-const DESCRIPTION = `
-  This is home
-`;
+const Home = () => {
+  const renderDescription = () => (
+    <React.Fragment>
+      <p>
+        Welcome to my new personal project!!!
+      </p>
 
-const Home = () => (
-  <div>
-    <EmptyView description={DESCRIPTION} />
-  </div>
-);
+      <p>
+        Ask the bot about me and try to recover the website with the info he will provide.
+      </p>
+
+      <p>
+        Every page has a progress bar which will indicate how much info you have already recovered
+        so you will know where to stop asking about some particular topic.
+      </p>
+
+      <p>
+        One more time, this project was created to learn about Chatbots
+        and to practice my full stack skills. I am happy at my current job and
+        I am not looking for a change.
+      </p>
+    </React.Fragment>
+  );
+
+  return (
+    <div className="home-page-container">
+      <EmptyView>{renderDescription()}</EmptyView>
+    </div>
+  );
+};
 
 export default Home;
