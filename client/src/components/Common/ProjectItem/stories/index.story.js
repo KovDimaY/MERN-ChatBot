513cc7de4d00a3711c058d0bbbbb60727b3370fe
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2019-04-27 23:04:54 +0200

    DEV-22 added knobs to the ProjectItem component stories

--- client/src/components/Common/ProjectItem/stories/index.story.js
+++ client/src/components/Common/ProjectItem/stories/index.story.js
@@ -1,18 +1,64 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text, array } from '@storybook/addon-knobs';
 
 import ProjectItem from '..';
 import { projects } from '../../../Pages/Projects/constants';
 
-const WrappedComponent = props => (
-  <div style={{ padding: '25px' }}>
-    <ProjectItem {...props} />
-  </div>
-);
-
 storiesOf('Common/ProjectItem', module)
-  .add('TripReviewer', () => WrappedComponent(projects[0]))
-  .add('SimpleChat', () => WrappedComponent(projects[1]))
-  .add('React-Highcharts Playground', () => WrappedComponent(projects[2]))
-  .add('Memory Game', () => WrappedComponent(projects[3]))
-  .add('KovDimaY', () => WrappedComponent(projects[4]));
+  .addDecorator(withKnobs)
+  .add('TripReviewer', () => (
+    <div style={{ padding: '25px' }}>
+      <ProjectItem
+        image={projects[0].image}
+        name={text('Name', projects[0].name)}
+        description={text('Description', projects[0].description)}
+        tools={array('Tools', projects[0].tools)}
+        url={text('URL', projects[0].url)}
+      />
+    </div>
+  ))
+  .add('SimpleChat', () => (
+    <div style={{ padding: '25px' }}>
+      <ProjectItem
+        image={projects[1].image}
+        name={text('Name', projects[1].name)}
+        description={text('Description', projects[1].description)}
+        tools={array('Tools', projects[1].tools)}
+        url={text('URL', projects[1].url)}
+      />
+    </div>
+  ))
+  .add('React-Highcharts Playground', () => (
+    <div style={{ padding: '25px' }}>
+      <ProjectItem
+        image={projects[2].image}
+        name={text('Name', projects[2].name)}
+        description={text('Description', projects[2].description)}
+        tools={array('Tools', projects[2].tools)}
+        url={text('URL', projects[2].url)}
+      />
+    </div>
+  ))
+  .add('Memory Game', () => (
+    <div style={{ padding: '25px' }}>
+      <ProjectItem
+        image={projects[3].image}
+        name={text('Name', projects[3].name)}
+        description={text('Description', projects[3].description)}
+        tools={array('Tools', projects[3].tools)}
+        url={text('URL', projects[3].url)}
+      />
+    </div>
+  ))
+  .add('KovDimaY', () => (
+    <div style={{ padding: '25px' }}>
+      <ProjectItem
+        image={projects[0].image}
+        name={text('Name', projects[4].name)}
+        description={text('Description', projects[4].description)}
+        tools={array('Tools', projects[4].tools)}
+        url={text('URL', projects[4].url)}
+      />
+    </div>
+  ));
