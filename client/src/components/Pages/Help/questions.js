60f256f600257bbc7c3d24aa5f8cc62a2c8bf4e8
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2019-09-23 21:49:12 +0200

    DEV-31 implemented Help page with the list of questions

---
+++ client/src/components/Pages/Help/questions.js
@@ -0,0 +1,55 @@
+const homepage = {
+  title: 'Home Page',
+  children: [
+    { title: 'What is your name?' },
+    { title: 'What is your profession?' },
+    { title: 'Where do you live?' },
+    { title: 'How can I contact you?' },
+    { title: 'How old are you?' },
+    { title: 'Where are you from?' },
+    { title: 'Tell me something about yourself' },
+    { title: 'What languages do you speak?' },
+    { title: 'What are your hobbies?' },
+    { title: 'Tell me more about [programming, sports, puzzles, numismatics, photography]' },
+  ],
+};
+
+const experience = {
+  title: 'Experience Page',
+  children: [
+    { title: 'Tell me about your working experiences?' },
+    { title: 'What is your position in [Cornerjob, Bigfinite, Enixan, MBAS, Icicle]?' },
+    { title: 'When did you work in [Cornerjob, Bigfinite, Enixan, MBAS, Icicle]?' },
+    { title: 'Describe your work in [Cornerjob, Bigfinite, Enixan, MBAS, Icicle]?' },
+    { title: 'What technologies did you use in [Cornerjob, Bigfinite]?' },
+    { title: 'What responsibilities did you have in [Enixan, MBAS]?' },
+  ],
+};
+
+const education = {
+  title: 'Education Page',
+  children: [
+    { title: 'Tell me about your education?' },
+    { title: 'What topic did you study during you [master, bachelor]?' },
+    { title: 'When did you get your [master, bachelor]?' },
+    { title: 'What [roles, activities] did you have while getting your [master, bachelor]?' },
+    { title: 'What additional education do you have?' },
+
+  ],
+};
+
+const projects = {
+  title: 'Projects Page',
+  children: [
+    { title: 'Tell me about your projects?' },
+    { title: 'I want to know more about [TripReviewer, SimpleChat, Highcharts, Memory Game, KovDimaY]?' },
+    { title: 'Which technologies did you use in [TripReviewer, SimpleChat, Highcharts, Memory Game, KovDimaY]?' },
+  ],
+};
+
+export const questions = [
+  homepage,
+  experience,
+  education,
+  projects,
+];
