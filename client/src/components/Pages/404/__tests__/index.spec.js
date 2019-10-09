09a37fde2b9d41b84dd46a9ed54239fc2cb74037
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2019-10-09 10:38:52 +0200

    DEV-36 fixed tests and coverage

---
+++ client/src/components/Pages/404/__tests__/index.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+
+import NotFound from '../../404';
+
+jest.mock('react-tilt', () => 'Tilt');
+jest.mock('react-router-dom', () => ({ Link: 'Link' }));
+
+const mockComponent = props => (
+  <NotFound {...props} />
+);
+
+describe('components/Pages/<NotFound />', () => {
+  it('should render component', () => {
+    const tree = create(mockComponent()).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
